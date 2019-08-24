9e8477532cd30476f3218e2724cc4e251770819e
julien.calenge@epitech.eu <julien.calenge@epitech.eu@epitech.eu>
2019-08-24 22:07:49 +0200

    Update help and accuracy

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from functools import partial
 from PIL import Image
 from math import sqrt
 from termcolor import colored
-from multiprocessing.dummy import Pool as ThreadPool
 import sys
 import cv2
 from pathlib import *
 from sys import stdout
-from time import clock
+from time import time
+import shutil
 import os
 
 RED = 0
@@ -21,22 +20,23 @@ GREY = 0
 
 
 def help():
-    print "Usage : -directory -type_of_picture -number_of_picture (starting from 1)"
-    exit(0)
+    print("python Yolanda [directory] [type_of_picture] [number_of_frames_per_second] [name] [video_source]")
+    exit()
 
 def main(argv):
     if (len(argv) > 1):
         if (argv[1] == '--help'):
             help()
     if (len(argv) != 6):
-        print colored("Error : Not enough arguments, --help for help", 'red')
-        exit(84)
-    print 'Initialisation...'
-    print colored("Getting Video...\n", 'yellow')
+        print (colored("Error : Not enough arguments, --help for help", 'red'))
+        exit()
+    t0 = time()
+    print ('Initialisation...')
+    print (colored("Getting Video...\n", 'yellow'))
     cam = cv2.VideoCapture(argv[5])
-    print colored("Success, '" + argv[4] + "' opened" , 'green')
+    print (colored("Success, '" + argv[4] + "' opened" , 'green'))
     cur_frame = 1
-    framerate = cam.get(cv2.cv.CV_CAP_PROP_FPS)
+    framerate = cam.get(cv2.CAP_PROP_FPS)
     framerate = framerate / int(argv[3])
     check_fr = 0
     ret, frame=cam.read()
@@ -44,7 +44,7 @@ def main(argv):
         if ret and check_fr == int(framerate):
             if (os.path.exists(argv[1] + str(cur_frame) + argv[2]) == False):
                 name = argv[1] + str(cur_frame) + argv[2]
-                print "File", colored(name, 'blue'), colored(" Created", 'green')
+                print ("File", colored(name, 'blue'), colored(" Created", 'green'))
                 cv2.imwrite(name, frame)
             check_fr = 0
             cur_frame += 1
@@ -54,7 +54,7 @@ def main(argv):
     bar_len = 100
     equal = colored('#', 'green')
     sep = colored('=', 'red')
-    seconds = clock()
+    seconds = time()
     colors = [[255, 0, 0, "RED"],
     [0, 255, 0, "GREEN"],
     [0, 0, 255, "BLUE"],
@@ -71,7 +71,7 @@ def main(argv):
     u = '%'
     trash = 0
     res = 1000
-    print colored("Video has been turned into images\n\n", "green")
+    print (colored("Video has been turned into images\n\n", "green"))
     path = argv[1] + '1' + argv[2]
     image = Image.open(path)
     pic = image.load()
@@ -84,29 +84,40 @@ def main(argv):
     ptr = 0
     y = size[0]
     x = size[1]
+    total_pix = y * x
     path = argv[1] + '%s' + argv[2]
     while (cm1 - 1 != cur_frame):
         if (os.path.exists(path % cm1)):
             trash += 1
         else:
-            print 'File ', colored(path % cm1, 'blue'), colored(" FAILED", 'red')
+            print ('File ', colored(path % cm1, 'blue'), colored(" FAILED", 'red'))
             check_files[ws] = path % cm1
             ws += 1
         cm1 += 1
     if (check_files[0] != 0):
-        print colored("Error: Invalid files", 'red', attrs=['bold'])
+        print (colored("Error: Invalid files", 'red', attrs=['bold']))
         ws = 0
-        print colored("Incriminated files: ", 'red', attrs=['bold'])
+        print (colored("Incriminated files: ", 'red', attrs=['bold']))
         while check_files[ws] != '0':
-            print colored('[', 'red'), colored(check_files[ws], 'blue'), colored(']', 'red')
+            print (colored('[', 'red'), colored(check_files[ws], 'blue'), colored(']', 'red'))
             ws += 1
-        exit(84)
-    print "Total files : ", colored(cm1 - 1, 'green', attrs=['blink'])
-    print colored("Colors being tested :", 'blue')
+        exit()
+    print ("Total files : ", colored(cm1 - 1, 'green', attrs=['blink']))
+    print (colored("Colors being tested :", 'blue'))
     while (ptr != 8):
-        print colored(colors[ptr][3], 'blue', attrs=['dark'])
+        print (colored(colors[ptr][3], 'blue', attrs=['dark']))
         ptr += 1
+    cm2 = cm * 100.00 / cm1
+    seconds = time() - t0
+    mins = seconds / 60.00
+    estim = total_pix * 0.000005 * cm1 / 60.00
+    print("Estimitated time: ", int(estim), " minutes")
+    filled_len = int(round(bar_len * cm2 / float(100)))
+    percents = round(100.0 * cm2 / float(100), 1)
+    bar = equal * filled_len + sep * (bar_len - filled_len)
     stdout.write('Evolution : \n')
+    sys.stdout.write('[%s] %s%s%s%.1f%s%s%s\r' % (bar, percents, '%', colored(" Time passed : ", 'green'), mins, " minutes", colored(" Image N*", 'blue'), colored(cm - 1, 'blue')))
+    sys.stdout.flush() 
     while (cm != cm1):
         image = Image.open(path % cm)
         pic = image.load()
@@ -138,29 +149,37 @@ def main(argv):
         cm += 1
         a = 1
         cm2 = cm * 100.00 / cm1
-        seconds = clock()
+        seconds = time() - t0
         mins = seconds / 60.00
         filled_len = int(round(bar_len * cm2 / float(100)))
         percents = round(100.0 * cm2 / float(100), 1)
         bar = equal * filled_len + sep * (bar_len - filled_len)
-        sys.stdout.write('[%s] %s%s%s%.1f%s%s%s\r' % (bar, percents, '%', colored(" Time passed : ", 'green'), mins, " minutes", colored(" Image N*", 'blue'), colored(cm - 1, 'blue')))
+        remain = (mins / percents) * (100 - percents)
+        remain = float("{0:.2f}".format(remain))
+        sys.stdout.write('[%s] %s%s%s%.1f%s%s%s%s%s%s\r' % (bar, percents, '%', colored(" Time passed : ", 'green'), mins, " minutes", colored(" Image N*", 'blue'), colored(cm - 1, 'blue'), colored(" Time Remaining: ", 'red'), colored(str(remain), 'red'), colored(' minutes', 'red')))
         sys.stdout.flush()
 
-    print colored("\n\nSUCCESS\n", 'green', attrs=['bold'])
-    print colored("Red : ", 'red'), "%.4f" % (RED * 100.00 / ((y * x) * (cm - 1))), "%"
-    print colored("Green : ", 'green'), "%.4f" % (GREEN * 100.00 / ((y * x) * (cm - 1))), "%"
-    print colored("Yellow : ", 'yellow'), "%.4f" % (YELLOW * 100.00 / ((y * x) * (cm - 1))), "%"
-    print colored("Blue : ", 'blue'), "%.4f" % (BLUE * 100.00 / ((y * x) * (cm - 1))), "%"
-    print colored("Purple : ", 'magenta'), "%.4f" % (PURPLE * 100.00 / ((y * x) * (cm - 1))), "%"
-    print colored("\n--- --- ---", 'red', attrs=['bold'])
-    print "\nBlack : ", "%.4f" % (BLACK * 100.00 / ((y * x) * (cm - 1))), "%"
-    print "White : ", "%.4f" % (WHITE * 100.00 / ((y * x) * (cm - 1))), "%"
-    print "Grey : ", "%.4f" % (GREY * 100.00 / ((y * x) * (cm - 1))), "%"
-    fd = open("db.txt", "rw+")
+    print (colored("\n\nSUCCESS\n", 'green', attrs=['bold']))
+    print (colored("Red : ", 'red'), "%.4f" % (RED * 100.00 / ((y * x) * (cm - 1))), "%")
+    print (colored("Green : ", 'green'), "%.4f" % (GREEN * 100.00 / ((y * x) * (cm - 1))), "%")
+    print (colored("Yellow : ", 'yellow'), "%.4f" % (YELLOW * 100.00 / ((y * x) * (cm - 1))), "%")
+    print (colored("Blue : ", 'blue'), "%.4f" % (BLUE * 100.00 / ((y * x) * (cm - 1))), "%")
+    print (colored("Purple : ", 'magenta'), "%.4f" % (PURPLE * 100.00 / ((y * x) * (cm - 1))), "%")
+    print (colored("\n--- --- ---", 'red', attrs=['bold']))
+    print ("\nBlack : ", "%.4f" % (BLACK * 100.00 / ((y * x) * (cm - 1))), "%")
+    print ("White : ", "%.4f" % (WHITE * 100.00 / ((y * x) * (cm - 1))), "%")
+    print ("Grey : ", "%.4f" % (GREY * 100.00 / ((y * x) * (cm - 1))), "%")
+    fd = open("db.txt", "r+")
     fd.seek(0, 2)
+    print (time() - t0)
     db_res = argv[4] + ' , ' + str((RED * 100.00 / ((y * x) * (cm - 1)))) + ' ; ' + str((GREEN * 100.00 / ((y * x) * (cm - 1)))) + ' ; ' + str((BLUE * 100.00 / ((y * x) * (cm - 1)))) + ' ; ' + str((PURPLE * 100.00 / ((y * x) * (cm - 1)))) + ' ; ' + str((GREY * 100.00 / ((y * x) * (cm - 1)))) + ' ; ' + str((WHITE * 100.00 / ((y * x) * (cm - 1)))) + ' ; ' + str((BLACK * 100.00 / ((y * x) * (cm - 1)))) + '\n'
     fd.write(db_res)
     fd.close()
+    for the_file in os.listdir("test/"):
+        file_path = os.path.join("test/", the_file)
+        if os.path.isfile(file_path):
+            os.unlink(file_path)
+    #go though data analysis (machine learning) to know which category/director this movie is from -> data
 
 if __name__ == "__main__":
     main(sys.argv)
