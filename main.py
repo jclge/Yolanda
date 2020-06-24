e7a8bb2c768af71a0d196ece7d31dbc24b40d8d8
jclge <julien.calenge@epitech.eu>
2020-06-24 09:02:20 +0200

    v 0.2

--- main.py
+++ main.py
@@ -4,22 +4,42 @@ from termcolor import colored
 import sys
 import cv2
 from pathlib import *
+import colorama
 from sys import stdout
-import numpy
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from sklearn import datasets
+from sklearn import metrics
+import tensorflow as tf
+import pandas as pd
 import random
 from time import time
 import shutil
+from numba import cuda
 import csv
 import os
 
 argv = None
+data = None
+RED = 0
+GREEN = 0
+BLUE = 0
+PURPLE = 0
+YELLOW = 0
+BLACK = 0
+WHITE = 0
+GREY = 0
 
 def help():
     print("python Yolanda [directory] [type_of_picture] [number_of_frames_per_second] [name] [video_source]")
     exit()
 
+def euclidean_difference(value_1, value_2, value_3, value_color_1, value_color_2, value_color_3):
+    return(sqrt(( 2 * (value_color_1 - value_1) ** 2) + (4 * (value_color_2 - value_1) ** 2) + (3 * (value_color_3 - value_3) ** 2)))
+
 class Yolanda:
     def __init__(self):
+        colorama.init()
         self.cam = None
         self.cur_frame = None
         self.framerate = None
@@ -61,34 +81,20 @@ class Yolanda:
         self.remain = None
         self.bar = None
         self.list_c = None
-        self.RED = 0
-        self.GREEN = 0
-        self.BLUE = 0
-        self.PURPLE = 0
-        self.YELLOW = 0
-        self.BLACK = 0
-        self.WHITE = 0
-        self.GREY = 0
 
-    def main(self):
+    def create_images(self):
         global argv
-        if (len(argv) > 1):
-            if (argv[1] == '--help'):
-                help()
-        if (len(argv) != 6):
-            print (colored("Error : Not enough arguments, --help for help", 'red'))
-            exit()
         print ('Initialisation...')
         print (colored("Getting Video...\n", 'yellow'))
         self.cam = cv2.VideoCapture(argv[5])
         print (colored("Success, '" + argv[4] + "' opened" , 'green'))
         self.cur_frame = 1
         self.framerate = self.cam.get(cv2.CAP_PROP_FPS)
-        self.framerate = self.framerate / int(argv[3])
+        self.framerate = self.framerate / float(argv[3])
         self.check_fr = 0
         self.ret, self.frame=self.cam.read()
         while self.ret:
-            if self.ret and self.check_fr == int(self.framerate):
+            if self.ret and self.check_fr == float(self.framerate):
                 if (os.path.exists(argv[1] + str(self.cur_frame) + argv[2]) == False):
                     self.name = argv[1] + str(self.cur_frame) + argv[2]
                     print ("File", colored(self.name, 'blue'), colored(" Created", 'green'))
@@ -102,6 +108,7 @@ class Yolanda:
         self.equal = colored('#', 'green')
         self.sep = colored('=', 'red')
         self.seconds = time()
+        print(colored("Video has been turned into images\n\n", "green"))
         self.colors = [[255, 0, 0, "RED"],
         [0, 255, 0, "GREEN"],
         [0, 0, 255, "BLUE"],
@@ -111,13 +118,23 @@ class Yolanda:
         [255, 255, 255, "WHITE"],
         [33, 33, 33, "GREY"]]
 
+    def error_management(self):
+        global argv
+        if (len(argv) > 1):
+            if (argv[1] == '--help'):
+                help()
+        if (len(argv) != 6):
+            print (colored("Error : Not enough arguments, --help for help", 'red'))
+            exit()
+
+    def initialize_main_variables(self):
+        global argv
         self.i = 1
         self.a = 1
         self.j = 0
         self.h = -1
         self.trash = 0
         self.res = 1000
-        print (colored("Video has been turned into images\n\n", "green"))
         self.path = argv[1] + '1' + argv[2]
         self.image = Image.open(self.path)
         self.pic = self.image.load()
@@ -132,6 +149,20 @@ class Yolanda:
         self.x = self.size[1]
         self.total_pix = self.y * self.x
         self.path = argv[1] + '%s' + argv[2]
+    
+    def print_results(self):
+        print (colored("\n\nSUCCESS\n", 'green', attrs=['bold']))
+        print (colored("Red : ", 'red'), "%.4f" % (RED * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
+        print (colored("Green : ", 'green'), "%.4f" % (GREEN * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
+        print (colored("Yellow : ", 'yellow'), "%.4f" % (YELLOW * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
+        print (colored("Blue : ", 'blue'), "%.4f" % (BLUE * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
+        print (colored("Purple : ", 'magenta'), "%.4f" % (PURPLE * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
+        print (colored("\n--- --- ---", 'red', attrs=['bold']))
+        print ("\nBlack : ", "%.4f" % (BLACK * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
+        print ("White : ", "%.4f" % (WHITE * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
+        print ("Grey : ", "%.4f" % (GREY * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
+
+    def check_files_integrity(self):
         while (self.cm1 - 1 != self.cur_frame):
             if (os.path.exists(self.path % self.cm1)):
                 self.trash += 1
@@ -148,11 +179,8 @@ class Yolanda:
                 print (colored('[', 'red'), colored(self.check_files[self.ws], 'blue'), colored(']', 'red'))
                 self.ws += 1
             exit()
-        print ("Total files : ", colored(self.cm1 - 1, 'green', attrs=['blink']))
-        print (colored("Colors being tested :", 'blue'))
-        while (self.ptr != 8):
-            print (colored(self.colors[self.ptr][3], 'blue', attrs=['dark']))
-            self.ptr += 1
+
+    def set_time(self):
         self.cm2 = self.cm * 100.00 / self.cm1
         self.t0 = time()
         self.seconds = time() - self.t0
@@ -165,6 +193,34 @@ class Yolanda:
         stdout.write('Evolution : \n')
         sys.stdout.write('[%s] %s%s%s%.1f%s%s%s\r' % (self.bar, self.percents, '%', colored(" Time passed : ", 'green'), self.mins, " minutes", colored(" Image N*", 'blue'), colored(self.cm - 1, 'blue')))
         sys.stdout.flush()
+
+    def set_colors(self):
+        globals()[self.colors[self.h][3]] += 1
+
+    def get_dataset(self, set):
+        global data
+        data = pd.read_csv(set, delimiter=',')
+        data = data.drop(['NAME'], axis=1)
+        data_to_analyse = data.drop(['TYPE'], axis=1)
+        data = data[['TYPE']].values.tolist()
+        tuples = [tuple(y) for y in data_to_analyse.values]
+        return (tuples)
+
+    def neirest_neighbors(self):
+        global data
+        tuples = self.get_dataset("db.csv")
+        nparray = np.array(tuples, dtype=None, copy=True, order='K', subok=False, ndmin=0)
+        data = np.ravel(data)
+        analysis = np.array(data, dtype=None, copy=True, order='K', subok=False, ndmin=0)
+        K = 3
+        model = KNeighborsClassifier(n_neighbors = K)
+        model.fit(nparray, analysis)
+        print (colored("It looks like it is from the genre: ", 'green'))
+        res = model.predict([[(RED * 100.00 / ((self.y * self.x) * (self.cm - 1))),(GREEN * 100.00 / ((self.y * self.x) * (self.cm - 1))),(BLUE * 100.00 / ((self.y * self.x) * (self.cm - 1))),(PURPLE * 100.00 / ((self.y * self.x) * (self.cm - 1))),(YELLOW * 100.00 / ((self.y * self.x) * (self.cm - 1))),(GREY * 100.00 / ((self.y * self.x) * (self.cm - 1))),(WHITE * 100.00 / ((self.y * self.x) * (self.cm - 1))),(BLACK * 100.00 / ((self.y * self.x) * (self.cm - 1)))]])
+        print(colored(res[0], 'green', attrs=['bold']))
+        return (res[0])
+
+    def main_loop(self):
         while (self.cm != self.cm1):
             self.image = Image.open(self.path % self.cm)
             self.pic = self.image.load()
@@ -181,12 +237,12 @@ class Yolanda:
                         self.h = 7
                         self.j = 5
                     while (self.j != 5):
-                        self.nb = sqrt(( 2 * (self.colors[self.j][0] - self.list_c[0]) ** 2) + (4 * (self.colors[self.j][1] - self.list_c[1]) ** 2) + (3 * (self.colors[self.j][2] - self.list_c[2]) ** 2))
+                        self.nb = euclidean_difference(self.list_c[0], self.list_c[1], self.list_c[2], self.colors[self.j][0], self.colors[self.j][1], self.colors[self.j][2])
                         if (self.nb < self.res):
                             self.res = self.nb
                             self.h = self.j
                         self.j += 1
-                    self.colors[self.h][3] += 1
+                    self.set_colors()
                     self.res = 1000
                     self.h = -1
                     self.j = 0
@@ -206,31 +262,40 @@ class Yolanda:
             sys.stdout.write('[%s] %s%s%s%.1f%s%s%s%s%s%s\r' % (self.bar, self.percents, '%', colored(" Time passed : ", 'green'), self.mins, " minutes", colored(" Image N*", 'blue'), colored(self.cm - 1, 'blue'), colored(" Time Remaining: ", 'red'), colored(str(self.remain), 'red'), colored(' minutes       ', 'red')))
             sys.stdout.flush()
 
-        print (colored("\n\nSUCCESS\n", 'green', attrs=['bold']))
-        print (colored("Red : ", 'red'), "%.4f" % (self.RED * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
-        print (colored("Green : ", 'green'), "%.4f" % (self.GREEN * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
-        print (colored("Yellow : ", 'yellow'), "%.4f" % (self.YELLOW * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
-        print (colored("Blue : ", 'blue'), "%.4f" % (self.BLUE * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
-        print (colored("Purple : ", 'magenta'), "%.4f" % (self.PURPLE * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
-        print (colored("\n--- --- ---", 'red', attrs=['bold']))
-        print ("\nBlack : ", "%.4f" % (self.BLACK * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
-        print ("White : ", "%.4f" % (self.WHITE * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
-        print ("Grey : ", "%.4f" % (self.GREY * 100.00 / ((self.y * self.x) * (self.cm - 1))), "%")
+    def main(self):
+        global argv
+        self.error_management()
+        self.create_images()
+        self.initialize_main_variables()
+        self.check_files_integrity()
+        print ("Total files : ", colored(self.cm1 - 1, 'green', attrs=['blink']))
+        print (colored("Colors being tested :", 'blue'))
+        while (self.ptr != 8):
+            print (colored(self.colors[self.ptr][3], 'blue', attrs=['dark']))
+            self.ptr += 1
+        self.set_time()
+        self.main_loop()
+        self.print_results()
+        result = self.neirest_neighbors()
         fd = open("db.csv", "r+")
         fd.seek(0, 2)
-        db_res = argv[4] + ',' + str((self.RED * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((self.GREEN * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((self.BLUE * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((self.PURPLE * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((self.YELLOW * 100.00 / ((self.y * self.x) * (self.cm - 1)))) +',' + str((self.GREY * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((self.WHITE * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((self.BLACK * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + '\n'
-        fd.write(db_res)
+        db_res = argv[4] + ',' + str((RED * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((GREEN * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((BLUE * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((PURPLE * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((YELLOW * 100.00 / ((self.y * self.x) * (self.cm - 1)))) +',' + str((GREY * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((WHITE * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str((BLACK * 100.00 / ((self.y * self.x) * (self.cm - 1)))) + ',' + str(result) + '\n'
+        if (input("\nWould you like to save this in the dataset, as if ? y/N: ") == 'y'):
+           fd.write(db_res)
+           print(colored('\nDone!', 'green'))
         fd.close()
-        for the_file in os.listdir("test/"):
-            file_path = os.path.join("test/", the_file)
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-        filename = 'db.csv'
-        raw_data = open(filename, 'rt')
-        reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE)
-        x = list(reader)
-        data = numpy.array(x)
 
 if __name__ == "__main__":
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[0],
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024),
+                tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPU,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            print(e)
     argv = sys.argv
     Yolanda().main()
